1688721d6bb7acf0e55e478d062845f3e77ab65a
Eugene Kos <mstrsid@live.com>
2022-07-09 20:38:30 +0300

    task done

--- js/script.js
+++ js/script.js
@@ -28,6 +28,7 @@ removeAds('.promo__adv');
 replaceGenre('.promo__genre');
 replaceBg('.promo__bg');
 createFilmList('.promo__interactive-list');
+addFilm('button', '.adding__input');
 
 function removeAds(blockClassName) {
 	const removedBlock = document.querySelector(blockClassName);
@@ -57,10 +58,49 @@ function createFilmList(elemSelector) {
 	movieDB['movies'].sort();
 	movieDB['movies'].forEach((item, i) => {
 		let filmElem = document.createElement('li');
-		filmElem.innerHTML = `<li class="promo__interactive-item">${i+1}. ${item}
+		filmElem.innerHTML = `<li class="promo__interactive-item">${i + 1}. ${item}
                             <div class="delete"></div>
                         </li>`;
 		elem.append(filmElem);
 	});
+	removeFilm('.delete');
 }
 
+function addFilm(buttonSelector, inputSelector) {
+	const btn = document.querySelector(buttonSelector);
+	const inp = document.querySelector(inputSelector);
+
+	btn.addEventListener('click', (e) => {
+		e.preventDefault();
+		movieDB['movies'].push(trimFilmName(inp.value));
+		createFilmList('.promo__interactive-list');
+		inp.value = '';
+		isFavourite('input');
+	});
+}
+
+function trimFilmName(filmName) {
+	return filmName.length > 21 ? `${filmName.substring(0, 20)}...` : filmName;
+}
+
+function isFavourite(checkBoxSelector) {
+	const chbx = document.querySelectorAll(checkBoxSelector);
+	chbx.forEach(item => {
+		if (item.type === 'checkbox' && item.checked) {
+			console.log('Добавляем любимый фильм!');
+			item.checked = false;
+		}
+	});
+}
+
+function removeFilm(removerSelector) {
+	const trash = document.querySelectorAll(removerSelector);
+	trash.forEach(item => {
+		item.addEventListener('click', () => {
+			let text = item.previousSibling.textContent.trim();
+			text = text.substring(3, text.length);
+			movieDB['movies'] = movieDB['movies'].filter(item => item !== text);
+			createFilmList('.promo__interactive-list');
+		})
+	});
+}
